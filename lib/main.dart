19c07e41e97db64d43a4708d0c47323dd17649b5
cordeirorayanne952 <cordeirorayanne952@gmail.com>
2025-01-02 18:23:49 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Layout',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Programa Layout'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+    required this.title,
+  });
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                //color: Colors.amber,
+                child: const Text('Layout Superior'),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                        //alignment: Alignment.center,
+                        //color: Colors.red,
+                        //child: const Text('Primeira Coluna'),
+                        ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(
+                            255, 121, 33, 243), // Cor do fundo
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.black, // Cor da borda
+                          width: 2, // Espessura da borda
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color:
+                                Colors.black.withOpacity(0.5), // Cor da sombra
+                            spreadRadius: 2, // Expansão da sombra
+                            blurRadius: 10, // Suavidade da sombra
+                            offset:
+                                const Offset(4, 4), // Posição da sombra (x, y)
+                          ),
+                        ],
+                      ),
+                      child: const Text('Meu aplicativo'),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                        //alignment: Alignment.center,
+                        //color: Colors.red,
+                        //child: const Text('TeceiraColuna'),
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.center,
+                //color: Colors.blue,
+                child: const Text('Layout inferior'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
